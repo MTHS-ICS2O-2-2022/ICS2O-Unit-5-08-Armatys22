b49284e567bed5ba244b31565452dd7931263927
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-05-21 16:43:08 +0000

    hgh

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: dom
+// Created by: Jakub Malhotra
 // Created on: May 2023
 // This file contains the JS functions for index.html
 
